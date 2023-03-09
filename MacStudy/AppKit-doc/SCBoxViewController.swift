52da6e738b7aeed9793588e45191b21f8cfed486
童玉龙 <lengain@qq.com>
2023-03-09 18:09:43 +0800

    NSBox

--- MacStudy/AppKit-doc/SCBoxViewController.swift
+++ MacStudy/AppKit-doc/SCBoxViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import Cocoa
-
+/// title : NSBox
+/// description : style
 class SCBoxViewController: SCBaseCodeViewController {
 
     override func viewDidLoad() {
@@ -14,4 +15,28 @@ class SCBoxViewController: SCBaseCodeViewController {
         // Do view setup here.
     }
     
+    override func exampleCodeView() {
+        
+        /// start
+        /// separator
+        let separatorBox = NSBox(frame: NSRect.init(x: 15, y: 15, width: 150, height: 1))
+        separatorBox.boxType = .separator
+        
+        let separatorBox2 = NSBox(frame: NSRect.init(x: 170, y: 15, width: 1, height: 150))
+        separatorBox2.boxType = .separator
+        
+        /// primary
+        let primaryBox = NSBox(frame: NSRect.init(x: 180, y: 15, width: 150, height: 150))
+        primaryBox.boxType = .primary
+        primaryBox.title = "Input Aera"
+        primaryBox.contentViewMargins = NSSize(width: 10, height: 5)
+        
+        primaryBox.contentView?.addSubview(NSTextField(frame: NSRect(x: 0, y: 0, width: 90, height: 70)))
+        
+        /// end
+        contentView.addSubview(separatorBox)
+        contentView.addSubview(separatorBox2)
+        contentView.addSubview(primaryBox)
+        
+    }
 }
